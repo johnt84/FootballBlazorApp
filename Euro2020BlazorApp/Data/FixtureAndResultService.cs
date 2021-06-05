109eb355f4a817702dfb9f9913aeb04801eefcac
John Tomlinson <johnt84@hotmail.co.uk>
2021-06-06 00:40:16 +0100

    Added Post Group scheduled fixtures

--- Euro2020BlazorApp/Data/FixtureAndResultService.cs
+++ Euro2020BlazorApp/Data/FixtureAndResultService.cs
@@ -1,5 +1,6 @@
 ﻿using Euro2020BlazorApp.Models;
 using Euro2020BlazorApp.Models.FootballData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -63,7 +64,7 @@ namespace Euro2020BlazorApp.Data
         {
             int timeZoneOffsetInMinutes = await _timeZoneOffsetService.GetLocalOffsetInMinutes();
 
-            return  _fixturesAndResultsFootballDataModel
+            var fixturesAndResults = _fixturesAndResultsFootballDataModel
                                         .matches
                                         .ToList()
                                         .Select(x => new FixtureAndResult()
@@ -90,6 +91,34 @@ namespace Euro2020BlazorApp.Data
                                             Result = GetResult(x.score?.winner ?? string.Empty)
                                         })
                                         .ToList();
+
+            return PopulateNotDrawnFixtures(fixturesAndResults);
+        }
+
+        private List<FixtureAndResult> PopulateNotDrawnFixtures(List<FixtureAndResult> fixturesAndResults)
+        {
+            var notDrawnFixturesAndResults = fixturesAndResults
+                                                .Where(x => string.IsNullOrEmpty(x.HomeTeam.Name)
+                                                            && string.IsNullOrEmpty(x.AwayTeam.Name))
+                                                .ToList();
+
+            var postGroupDraws = GetPostGroupDraws();
+
+            notDrawnFixturesAndResults.ForEach(x => x.HomeTeam.Name = postGroupDraws
+                                                                          .Where(y => y.FixtureDate == x.FixtureDate)
+                                                                          .Select(x => x.HomeDraw)
+                                                                          .FirstOrDefault());
+
+            notDrawnFixturesAndResults.ForEach(x => x.AwayTeam.Name = postGroupDraws
+                                                                          .Where(y => y.FixtureDate == x.FixtureDate)
+                                                                          .Select(x => x.AwayDraw)
+                                                                          .FirstOrDefault());
+
+            fixturesAndResults
+                .RemoveAll(x => string.IsNullOrEmpty(x.HomeTeam.Name)
+                            && string.IsNullOrEmpty(x.AwayTeam.Name));
+
+            return fixturesAndResults;
         }
 
         private Stage GetStage(string stageName)
@@ -163,6 +192,118 @@ namespace Euro2020BlazorApp.Data
             }
 
             return result;
+        }
+
+        private List<PostGroupDraw> GetPostGroupDraws()
+        {
+            return new List<PostGroupDraw>()
+            {
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,26,17,0,0),
+                    HomeDraw = "Group A Second Place",
+                    AwayDraw = "Group B Second Place",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,26,20,0,0),
+                    HomeDraw = "Group A Winner",
+                    AwayDraw = "Group C Second Place",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,27,17,0,0),
+                    HomeDraw = "Group C Winner",
+                    AwayDraw = "Third Place Group D/E/F",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,27,20,0,0),
+                    HomeDraw = "Group B Winner",
+                    AwayDraw = "Third Place Group A/D/E/F",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,28,17,0,0),
+                    HomeDraw = "Group D Second Place",
+                    AwayDraw = "Group E Second Place",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,28,20,0,0),
+                    HomeDraw = "Group F Winner",
+                    AwayDraw = "Third Place Group A/B/C",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,29,17,0,0),
+                    HomeDraw = "Group D Winner",
+                    AwayDraw = "Group F Second Place",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Round_of_16,
+                    FixtureDate = new DateTime(2021,6,29,20,0,0),
+                    HomeDraw = "Group E Winner",
+                    AwayDraw = "Third Place Group A/B/C/D",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Quarter_Final,
+                    FixtureDate = new DateTime(2021,7,2,17,0,0),
+                    HomeDraw = "Winner Round of 16 match 6",
+                    AwayDraw = "Winner Round of 16 match 5",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Quarter_Final,
+                    FixtureDate = new DateTime(2021,7,2,20,0,0),
+                    HomeDraw = "Winner Round of 16 match 4",
+                    AwayDraw = "Winner Round of 16 match 2",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Quarter_Final,
+                    FixtureDate = new DateTime(2021,7,3,17,0,0),
+                    HomeDraw = "Winner Round of 16 match 3",
+                    AwayDraw = "Winner Round of 16 match 1",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Quarter_Final,
+                    FixtureDate = new DateTime(2021,7,3,20,0,0),
+                    HomeDraw = "Winner Round of 16 match 8",
+                    AwayDraw = "Winner Round of 16 match 7",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Semi_Final,
+                    FixtureDate = new DateTime(2021,7,6,20,0,0),
+                    HomeDraw = "Winner Quarter Final 2",
+                    AwayDraw = "Winner Quarter Final 1",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Semi_Final,
+                    FixtureDate = new DateTime(2021,7,7,20,0,0),
+                    HomeDraw = "Winner Quarter Final 4",
+                    AwayDraw = "Winner Quarter Final 3",
+                },
+                new PostGroupDraw()
+                {
+                    Stage = Stage.Final,
+                    FixtureDate = new DateTime(2021,7,11,20,0,0),
+                    HomeDraw = "Winner Semi Final 1",
+                    AwayDraw = "Winner Semi Final 2",
+                },
+            };
         }
     }
 }
---
+++ Euro2020BlazorApp/Models/PostGroupDraw.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static Euro2020BlazorApp.Models.Enums.Enums;
+
+namespace Euro2020BlazorApp.Models
+{
+    public class PostGroupDraw
+    {
+        public Stage Stage { get; set; }
+        public DateTime FixtureDate { get; set; }
+        public string HomeDraw { get; set; }
+        public string AwayDraw { get; set; }
+    }
+}
