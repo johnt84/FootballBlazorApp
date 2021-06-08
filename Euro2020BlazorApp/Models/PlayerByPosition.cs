ceb9261bc90a5e6f9f7886cadd147e8426619ef1
John Tomlinson <johnt84@hotmail.co.uk>
2021-06-08 14:06:12 +0100

    Reverted previous files checked in on error

--- Euro2020BlazorApp/Models/PlayerByPosition.cs
+++ Euro2020BlazorApp/Models/PlayerByPosition.cs
@@ -5,6 +5,7 @@ namespace Euro2020BlazorApp.Models
     public class PlayerByPosition
     {
         public string Position { get; set; }
+        public int SortOrder { get; set; }
         public List<Player> Players { get; set; }
     }
 }
