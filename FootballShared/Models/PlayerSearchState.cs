fc800d7a57b89e968ca2ee3e0a09151fffbfdeb7
John Tomlinson <jtomlinson@brightree.com>
2022-10-11 15:04:28 +0100

    Missing PlayerSearchState from initial commit

---
+++ FootballShared/Models/PlayerSearchState.cs
@@ -0,0 +1,8 @@
+﻿namespace FootballShared.Models
+{
+    public class PlayerSearchState
+    {
+        public PlayerSearchCriteria PlayerSearchCriteria { get; set; }
+        public DateTime LastRefreshTime { get; set; }
+    }
+}
